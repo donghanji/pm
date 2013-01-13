d1c7e28ca2050a74cd192c01f44bd0d64fd2f41d
donghanji <l.x-xiong@163.com>
2013-01-12 19:24:10 -0800

    更改os模块，实现桌面浏览器信息获取； 修正jquery不能正常获取safari版本号的bug;

--- samples/os/os.js
+++ samples/os/os.js
@@ -9,28 +9,39 @@
 	}
 	//userAgent regexp
 	var Exp_USERAGENT={
-		WEBKIT:/WebKit\/([\d.]+)/,
-		OPERA:/Opera Mobi/,
-		ANDROID:/(Android)\s+([\d.]+)/,
-		IPAD:/(iPad).*OS\s([\d_]+)/,
-		IPHONE:/(iPhone\sOS)\s([\d_]+)/,
-		WEBOS:/(webOS|hpwOS)[\s\/]([\d.]+)/,
-		TOUCHPAD:/TouchPad/,
-		BLACKBERRY:/(BlackBerry).*Version\/([\d.]+)/,
-		FENNEC:/fennec/i
+		WEBKIT:/webkit[\/]([\w.]+)/,
+		ANDROID:/(android)\s+([\d.]+)/,
+		IPAD:/(ipad).*os\s([\d_]+)/,
+		IPHONE:/(iphone\sos)\s([\d_]+)/,
+		WEBOS:/(webos|hpwos)[\s\/]([\d.]+)/,
+		TOUCHPAD:/touchpad/,
+		BLACKBERRY:/(blackberry).*version\/([\d.]+)/,
+		FENNEC:/fennec/i,
+		MOZILLA:/(mozilla)(?:.*? rv:([\w.]+)|)/,
+		CHROME:/(chrome)[\/]([\w.]+)/,
+		OPERA:/(opera)(?:.*version|)[\/]([\w.]+)/,
+		SAFARI:/(safari)(?:.*version|)[\/]([\w.]+)/,
+		MSIE:/(msie) ([\w.]+)/
 	};
 	//define os module
 	module.declare('os',function(require,exports,module){
 		function detect(ua){
 			var os={},
+				//mobile
 				webkit = ua.match(Exp_USERAGENT.WEBKIT),
 				android = ua.match(Exp_USERAGENT.ANDROID),
-				ipad = ua.match(Exp_USERAGENT.IPAD),
 				iphone = !ipad && ua.match(Exp_USERAGENT.IPHONE),
 				webos = ua.match(Exp_USERAGENT.WEBOS),
+				blackberry = ua.match(Exp_USERAGENT.BLACKBERRY),
+				//pad
+				ipad = ua.match(Exp_USERAGENT.IPAD),
 				touchpad = webos && ua.match(Exp_USERAGENT.TOUCHPAD),
-				blackberry = ua.match(Exp_USERAGENT.BLACKBERRY);
-
+				//web
+				chrome = ua.match(Exp_USERAGENT.CHROME),
+				opera = ua.match(Exp_USERAGENT.OPERA),
+				msie = ua.match(Exp_USERAGENT.MSIE),
+				safari = (ua+ua.replace(Exp_USERAGENT.SAFARI,'')).match(Exp_USERAGENT.SAFARI),//modify the jquery bug
+				mozilla = ua.match(Exp_USERAGENT.MOZILLA);
 			if (android) os.android = true, os.version = android[2];
 			if (iphone) os.ios = true, os.version = iphone[2].replace(/_/g, '.'), os.iphone = true;
 			if (ipad) os.ios = true, os.version = ipad[2].replace(/_/g, '.'), os.ipad = true;
@@ -39,9 +50,16 @@
 			if (blackberry) os.blackberry = true, os.version = blackberry[2];
 			if (!(android || iphone || ipad || webos || touchpad || blackberry)) os.desktop=true,os.version=null;
 			
+			if (webkit) os.webkit=true;
+			if(os.desktop){
+				var match=chrome||safari||opera||msie||ua.indexOf('compatible') < 0 && mozilla;
+				os[match[1]] = true;
+				os['version'] = match[2];
+			}
+			
 			return os;
 		};
 		
-		return detect(navigator.userAgent);
+		return detect(navigator.userAgent.toLowerCase());
 	});
 })();

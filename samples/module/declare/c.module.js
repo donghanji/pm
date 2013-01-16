92ec6fd8aeba01147c02a1327af81b44c8ba1fb7
donghanji <l.x-xiong@163.com>
2013-01-16 05:27:15 -0800

    增加module.declare的使用实例；详细介绍模块的定义、模块的请求等；

---
+++ samples/module/declare/c.module.js
@@ -0,0 +1,5 @@
+(function(){
+	module.declare('c.module',function(require,exports){
+		exports.foo='c module';
+	});
+})();
---
+++ samples/module/declare/d.module.js
@@ -0,0 +1,5 @@
+(function(){
+	module.declare('d.module',function(require,exports){
+		exports.foo='d module';
+	});
+})();
